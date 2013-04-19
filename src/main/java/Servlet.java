bb37fdc7034bdf11d7c218cef5e956c7fb62c203
eleve <eleve@gl.(none)>
2013-04-19 16:21:20 +0200

    servlet

---
+++ src/main/java/Servlet.java
@@ -0,0 +1,25 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: eleve
+ * Date: 19/04/13
+ * Time: 16:09
+ * To change this template use File | Settings | File Templates.
+ */
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+    public class Servlet extends HttpServlet {
+
+        protected void doGet(HttpServletRequest req, HttpServletResponse res)
+                throws ServletException, IOException {
+            PrintWriter writer = res.getWriter();
+
+            writer.println("Hello World !!!");
+        }
+
+    }
